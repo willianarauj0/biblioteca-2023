0f006e91458b80a62a28f646ab4b5c510ec1a666
marcoweb <marcocarvalho.web@gmail.com>
2023-09-14 14:29:47 +0000

    Update - Delete (Generos)

--- app/src/main/java/application/controller/GeneroController.java
+++ app/src/main/java/application/controller/GeneroController.java
@@ -1,5 +1,7 @@
 package application.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -33,6 +35,53 @@ public class GeneroController {
         genero.setNome(nome);
 
         generoRepo.save(genero);
+
+        return "redirect:/genero/list";
+    }
+
+    @RequestMapping("/update")
+    public String update(Model model, @RequestParam("id") int id) {
+        Optional<Genero> genero = generoRepo.findById(id);
+
+        if(genero.isPresent()) {
+            model.addAttribute("genero", genero.get());
+            return "/genero/update";
+        }
+
+        return "redirect:/genero/list";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String update(
+        @RequestParam("id") int id,
+        @RequestParam("nome") String nome
+    ) {
+        Optional<Genero> genero = generoRepo.findById(id);
+
+        if(genero.isPresent()) {
+            genero.get().setNome(nome);
+
+            generoRepo.save(genero.get());
+        }
+        
+        return "redirect:/genero/list";
+    }
+
+    @RequestMapping("/delete")
+    public String delete(Model model, @RequestParam("id") int id) {
+        Optional<Genero> genero = generoRepo.findById(id);
+
+        if(genero.isPresent()) {
+            model.addAttribute("genero", genero.get());
+            return "/genero/delete";
+        }
+
+        return "redirect:/genero/list";
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public String delete(@RequestParam("id") int id) {
+        generoRepo.deleteById(id);
 
         return "redirect:/genero/list";
     }
