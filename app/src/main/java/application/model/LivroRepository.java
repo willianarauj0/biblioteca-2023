e502ba0945a386a57580ca9fec4061be6582d274
willianarauj0 araujo <willianeves66@gmail.com>
2023-11-07 17:20:17 +0000

    up

---
+++ app/src/main/java/application/model/LivroRepository.java
@@ -0,0 +1,10 @@
+package application.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LivroRepository extends CrudRepository<Livro, Integer> {
+    
+    
+
+    
+}
